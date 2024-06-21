c997d5569650b49413bf6e4bb226ba1af09baa14
NADEEM SHAIK <shaiknadeem2003@gmail.com>
2024-06-22 01:46:16 +0530

    Update Navbar.js

--- client/flysmart/srC/components/Navbar.js
+++ client/flysmart/srC/components/Navbar.js
@@ -1,10 +1,33 @@
 import React from 'react'
+//importing the logo from the images folder 
+import logo from './images/airplane.png'
+import Example  from './Dropdown'
+
 
 export default function Navbar() {
   return (
-    <nav className='bg-gray-200 shadow shadow-gray-300 w-100 px-8 md:px-auto'>
+    <nav className='bg-gray-900 shadow shadow-gray-300 w-100 px-8 md:px-auto'>
+        <div className='md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap'>
 
-      hello
+          {/* Logo */}
+          <div className="text-indigo-500 md:order-1">
+              <img className='logo' src={logo} alt='flight-logo'width={40} height={40} ></img>
+            </div>
+
+          <div className='text-red-100 order-3 w-full md:w-auto md:order-2'>
+            <ul className='flex font-semibold justify-between'>
+              <li className='md:px-4 md:py-2 hover:text-red-500'><a href='#'>Dashboard</a></li>
+              <li className='md:px-4 md:py-2 hover:text-red-500'><a href='#'>Coupons</a></li>
+              <li className='md:px-4 md:py-2 hover:text-red-500'><a href='#'>About</a></li>
+             
+            </ul>
+          </div>
+
+          <div className='order-2 md:order-3'>
+         
+            <Example/>
+          </div>
+        </div>
     </nav>
   )
 }
