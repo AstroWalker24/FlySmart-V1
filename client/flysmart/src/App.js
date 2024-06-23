f84b42c34446a2cae3f87de414231f1fb1292c24
snadeem03 <shaiknadeem2003@gmail.com>
2024-06-23 09:45:21 +0530

    Add comments to clarify complex logic

--- client/flysmart/src/App.js
+++ client/flysmart/src/App.js
@@ -1,16 +1,23 @@
 import React from 'react'
+
+// importing the global CSS file
 import './index.css'
+
+// importing the components
 import Navbar from './components/Navbar'
 import Carousel from './components/Carousel'
 import FlightSearch from './components/FlightSearch'
 
+// importing react Hooks
+import {useState,useEffect} from 'react';
+
+// importing react-router-dom
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 export default function App() {
   return (
     <>
-    {/* rendering navbar */}
     <Navbar/>
     <Carousel />
     <FlightSearch />
